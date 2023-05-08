dad7a5373493a1631a2ca403a8f40a9d9de109bd
Natan henrique <105169542+ProgrammerEspiffy@users.noreply.github.com>
2023-05-08 19:09:00 -0300

    Lançamento no Git Hub
    Em desenvolvimento

---
+++ Carlos Marquinhos/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 Mover = Vector3.forward;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mover Veículo Pra Frente
+        transform.Translate(Vector3.forward);
+        trans
+    }
+}
