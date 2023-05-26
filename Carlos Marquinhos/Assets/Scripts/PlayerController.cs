ace1d51a94d85e586181a48af4b3cef6debd1f26
Natan henrique <105169542+ProgrammerEspiffy@users.noreply.github.com>
2023-05-26 14:24:09 -0300

    the game is finished
    Autoexplaned

--- Carlos Marquinhos/Assets/Scripts/PlayerController.cs
+++ Carlos Marquinhos/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,29 @@
-using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector3 Mover = Vector3.forward;
-    // Start is called before the first frame update
+    //Variáveis do veículo
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     void Start()
     {
-        
+  
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        //Mover Veículo Pra Frente
-        transform.Translate(Vector3.forward);
-        trans
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Mover o veículo pra frente
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotacionar veículo
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
